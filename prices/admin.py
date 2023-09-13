7232270aff224fe7be51b2ac3bd27cda5175ae32
Clayton Tedesco <ctedesco@usp.br>
2023-09-12 23:06:50 -0300

    Initial models created

--- prices/admin.py
+++ prices/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Market, Price, Product
+
+
+@admin.register(Market)
+class AvaliacaoAdmin(admin.ModelAdmin):
+    list_display = ("id", "marketName", "city", "adress")
+    search_fields = ("marketName", "city")
+
+
+@admin.register(Product)
+class AvaliacaoAdmin(admin.ModelAdmin):
+    list_display = ("id", "productName", "brand", "category")
+    search_fields = ("productName", "brand", "category")
+
+
+@admin.register(Price)
+class CursoAdmin(admin.ModelAdmin):
+    list_display = ("id", "id_product", "id_market", "value")
+    list_editable = ("id_product",)
+    list_per_page = 20
+    search_fields = ("id_product",)
--- prices/models.py
+++ prices/models.py
@@ -1,3 +1,54 @@
 from django.db import models
 
-# Create your models here.
+
+class Base(models.Model):
+    create = models.DateTimeField(auto_now_add=True)
+    update = models.DateTimeField(auto_now=True)
+
+
+class Market(models.Model):
+    marketName = models.CharField(max_length=100, verbose_name="Name of the Market")
+    city = models.CharField(max_length=100)
+    adress = models.CharField(max_length=100, verbose_name="Adress")
+    obs = models.TextField(blank=True, null=True, verbose_name="Observation")
+
+    def __str__(self):
+        return self.marketName
+
+
+class Product(Base):
+    productName = models.CharField(max_length=100, verbose_name="Name of the Product")
+    brand = models.CharField(max_length=100)
+    category = models.CharField(
+        choices=(
+            ("G", "Grains"),
+            ("P", "Meat"),
+            ("S", "Spices"),
+            ("F", "Fruits"),
+            ("V", "Vegatables"),
+            ("H", "Hygiene"),
+            ("C", "Cleaning"),
+            ("B", "Beverages"),
+            ("D", "Candies"),
+        ),
+        max_length=1,
+    )
+    obs = models.TextField(blank=True, null=True, verbose_name="Observation")
+
+    def __str__(self):
+        return self.productName
+
+
+class Price(Base):
+    class Meta:
+        verbose_name_plural = "Prices"
+
+    id_market = models.ForeignKey(Market, on_delete=models.CASCADE)
+    id_product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    value = models.DecimalField(max_digits=6, decimal_places=2)
+
+
+class Purchases(Base):
+    id_product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    id_price = models.ForeignKey(Price, on_delete=models.CASCADE)
+    purchaseDate = models.DateField(blank=True, null=True)
